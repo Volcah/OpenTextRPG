0ae0cc5442b58708ce8cf1ff96eb900c21374b05
Volcah <thisisvolca@gmail.com>
2019-03-16 22:48:02 +0100

    Update Location.cpp

--- Source/Location.cpp
+++ Source/Location.cpp
@@ -2,17 +2,23 @@
 
 Location::Location()
 {
-    for(int x = 0; x < 10; x++)
+    for(int x = 0; x < 10; x++) {
         Dungeon1[x][0] = "X";
-    for(int y = 1; y < 10; y++)
+    }
+    for(int y = 1; y < 10; y++) {
         Dungeon1[0][y] = "X";
-    for(int x = 1; x < 9; x++)
-        for(int y = 1; y < 9; y++)
+    }
+    for(int x = 1; x < 9; x++) {
+        for(int y = 1; y < 9; y++) {
             Dungeon1[x][y] = " ";
-    for(int x = 1; x < 10; x++)
+        }
+    }
+    for(int x = 1; x < 10; x++) {
         Dungeon1[x][9] = "X";
-    for(int y = 1; y < 10; y++)
+    }
+    for(int y = 1; y < 10; y++) {
         Dungeon1[9][y] = "X";
+    }
     Dungeon1[6][5] = "X";
     Dungeon1[4][2] = "X";
 }
