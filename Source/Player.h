e2dadb31cca3588bbbbd18aedda47ac1d4c31012
Volcah <thisisvolca@gmail.com>
2019-03-16 22:23:45 +0100

    Update Player.h

--- Source/Player.h
+++ Source/Player.h
@@ -9,6 +9,17 @@ public:
 	Player() = default;
 	virtual compl Player() = default;
 	
-	string name, surname, playerclass;
-	int hp, atk, mag, exp, maxHp, level, nextlevel, actualZone, x, y;
+	string name = "PlayerName";
+	string surname = "PlayerSurname";
+	string playerclass = "Test";
+	int hp = 50;
+	int atk = 20;
+	int mag = 20;
+	int exp = 0;
+	int maxHp = 50;
+	int level = 1;
+	int nextlevel = 15;
+	int actualZone = 1;
+	int x = 3;
+	int y = 3;
 };
