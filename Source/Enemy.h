3b6c8c6be44b46a4d1aa8b24bfc7ad604b806d5e
Volcah <thisisvolca@gmail.com>
2019-03-16 21:53:57 +0100

    Update Enemy.h

--- Source/Enemy.h
+++ Source/Enemy.h
@@ -9,13 +9,18 @@ public:
 	Enemy() = default;
 	virtual compl Enemy() = default;
 	
-	string name;
-	int hp, atk, mag, exp, maxHp, level;
+	string name = "DefaultName";
+	int hp = 50;
+	int maxHp = hp;
+	int atk = 20;
+	int mag = 20;
+	int exp = 25
+	int level = 1;
 };
 
 class Dummy : public Enemy {
 public:
-	Dummy() = default;
+	Dummy();
 	virtual compl Dummy() = default;
 };
 
