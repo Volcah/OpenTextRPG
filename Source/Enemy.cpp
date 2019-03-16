ac3060275eed940983c6d3647481daeaae1b57f5
Volcah <thisisvolca@gmail.com>
2019-03-16 21:56:27 +0100

    Update Enemy.cpp

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -1,22 +1,10 @@
 #include "Enemy.h"
 
-Enemy::Enemy()
-{
-    name = "EnemyTest";
-    hp = 50;
-	atk = 20;
-	mag = 20;
-	exp = 25;
-	maxHp = hp;
-	level = 1;
-}
-
-Dummy::Dummy()
-{
-    name = "Dummy";
-    hp = 105;
+Dummy::Dummy() {
+	name = "Dummy";
+	hp = 105;
+	maxHp = 105;
 	atk = 1;
 	level = 1;
 	exp = 15;
-	maxHp = 105;
 }
