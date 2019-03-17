d9b3c65cea3a98c8e6f77f0a628c3be7abd1b6cd
Volcah <thisisvolca@gmail.com>
2019-03-17 18:26:45 +0100

    Update Item.cpp

--- Source/Item.cpp
+++ Source/Item.cpp
@@ -2,34 +2,36 @@
 #include "Game.h"
 
 void Item::Add(Item item) {
-    if(item.num < 10)
-        item.num++;
+	if(item.num < 10) {
+		item.num++;
+    }
 }
 
 void Item::Remove(Item item) {
-    if(item.num > 0)
-        item.num--;
+	if(item.num > 0) {
+		item.num--;
+    }
 }
 
 void Item::Show() {
-    rlutil::cls();
-    std::cout << "Name: " << name << std::endl;
-    std::cout << "Description: " << description << std::endl;
-    std::cout << "Value: " << value << std::endl;
-
+	rlutil::cls();
+	std::cout << "Name: " << name << std::endl;
+	std::cout << "Description: " << description << std::endl;
+	std::cout << "Value: " << value << std::endl;
 }
 
 Cure::Cure(string const& newName, string const& newDescription, int newValue, int newCurePoints) {
-    name = newName;
-    description = newDescription;
-    value = newValue;
-    CurePoints = newCurePoints;
+	name = newName;
+	description = newDescription;
+	value = newValue;
+	CurePoints = newCurePoints;
 }
 
 void Cure::Use(Player player) {
-    player.hp += Cure::CurePoints;
-    if(player.hp > player.maxHp)
-        player.hp = player.maxHp;
-    std::cout << player.name << " gained " << Cure::CurePoints << " HP.\n";
-    std::cout << player.name << " HP = " << player.hp << std::endl;
+	player.hp += Cure::CurePoints;
+	if(player.hp > player.maxHp) {
+		player.hp = player.maxHp;
+    }
+	std::cout << player.name << " gained " << Cure::CurePoints << " HP.\n";
+	std::cout << player.name << " HP = " << player.hp << std::endl;
 }
