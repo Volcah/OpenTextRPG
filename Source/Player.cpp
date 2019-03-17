9169ee54d22e29dd44abedbc56267f44bcf30315
Volcah <thisisvolca@gmail.com>
2019-03-17 19:28:54 +0100

    Update Player.cpp

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -4,7 +4,7 @@ void Player::Use(Cure c) {
 	hp += c.CurePoints;
 	if(hp > maxHp) {
 		hp = maxHp;
-    }
+	}
 	std::cout << name << " gained " << c.CurePoints << " HP.\n";
 	std::cout << name << " HP = " << hp << std::endl;
 }
